363b8c3640d6ebf30db2114b4487fe45b04f1a19
temptado <jason1287712@gmail.com>
2021-09-02 19:03:20 -0400

    added and tested route for updated category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -10,8 +10,7 @@ router.get('/', async (req, res) => {
     });
     res.status(200).json(categoriesData);
   } catch (err) {
-    console.log(err);
-    // res.status(500).json(err);
+    res.status(500).json(err);
   }
 });
 
@@ -38,14 +37,29 @@ router.post('/', async (req, res) => {
   // create a new category
   try {
     const newCategoryData = await Category.create(req.body);
-    res.status(200).json(newCategoryData);
+    res.status(201).json(newCategoryData);
   } catch (err) {
     res.status(400).json(err);
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const updatedCategory = await Category.update(
+      {
+        category_name: req.body.category_name
+      },
+      {
+        where: {
+          id: req.params.id
+        }
+      }
+    );
+    res.status(201).json(updatedCategory);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
