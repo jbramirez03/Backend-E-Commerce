c376ec7287dbe661d13df476057b06aaddeb08d0
temptado <jason1287712@gmail.com>
2021-09-03 19:31:51 -0400

    added comments to the categories route file

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,25 +1,33 @@
+// Use router in order to store the routes for category model
 const router = require('express').Router();
+// Require the models that include category or are associated to the category model
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 router.get('/', async (req, res) => {
+  // Use Try/Catch in order to catch errors while using await on functions that contain a promise
   try {
+    // Variable that uses sequelize to find all of the data in the 'Category' model/table
     const categoriesData = await Category.findAll({
+      // Using include in order to get products that contain the returned categories Id
       include: [{ model: Product, as: 'products' }]
     });
+    // Set the status to a 200 if there are no errors with the code or the server then return all of the categories through the defined variable
     res.status(200).json(categoriesData);
+    // If there is any errors they will be displayed in the catch by setting the status to a 500 and returning the error
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get('/:id', async (req, res) => {
   // find one category by its `id` value
+router.get('/:id', async (req, res) => {
   try {
+    // Set a variable that contains a single Category's data using the findByPk method while also using the req.params.id as the desired category to view
     const singleCategoryData = await Category.findByPk(req.params.id, {
       include: [{ model: Product, as: 'products' }]
     });
-
+    // If there is no category with the id set in the url then a 404 status comes back with a message formatted in json
     if (!singleCategoryData) {
       res.status(404).json({ message: 'No category found with that id.' })
       return;
@@ -31,9 +39,10 @@ router.get('/:id', async (req, res) => {
   }
 });
 
+// Route to create a new category
 router.post('/', async (req, res) => {
-  // create a new category
   try {
+    // Set a variable that contains the new category information through the use of the create mehtod in sequelize and the req.body
     const newCategoryData = await Category.create(req.body);
     res.status(201).json(newCategoryData);
   } catch (err) {
@@ -41,14 +50,16 @@ router.post('/', async (req, res) => {
   }
 });
 
+  // Route update a category by its `id` value
 router.put('/:id', async (req, res) => {
-  // update a category by its `id` value
   try {
+    // Define a variable containing the updated category information through the use of the update sequelize method and defining which files can be update
     const updatedCategory = await Category.update(
       {
         category_name: req.body.category_name
       },
       {
+        // The where is defined as the req.params.id in order to specify which category to update
         where: {
           id: req.params.id
         }
@@ -66,9 +77,10 @@ router.put('/:id', async (req, res) => {
   }
 });
 
+// Route to delete a category based on its 'id' value
 router.delete('/:id', async (req, res) => {
-  // delete a category by its `id` value
   try {
+    // Similar to the update method in sequelize you define a where object in the destroy method in sequelize and that where being defined as the req.params.id
     const deletedCategory = await Category.destroy({
       where: {
         id: req.params.id
@@ -85,4 +97,5 @@ router.delete('/:id', async (req, res) => {
   }
 });
 
+// Export all the routes through the use of the router
 module.exports = router;
