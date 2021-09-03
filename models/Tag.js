d29c150dfd1910e5a32a5584faf8f695272a8542
temptado <jason1287712@gmail.com>
2021-09-03 19:06:37 -0400

    added comments to tag model file

--- models/Tag.js
+++ models/Tag.js
@@ -1,9 +1,13 @@
+// Require parts of sequelize library needed in order to create a new model
 const { Model, DataTypes } = require('sequelize');
 
+// Import the connection to the database from the config file
 const sequelize = require('../config/connection.js');
 
+// Define tag as a new model by extending the model class in sequelize
 class Tag extends Model {}
 
+// Initialize the tag model by setting up the fields and rules
 Tag.init(
   {
     id: {
@@ -25,4 +29,5 @@ Tag.init(
   }
 );
 
+// Export the model in order to be used in index to define association to other models and in routes
 module.exports = Tag;
