d87224c8c91a8a3dcc85cee85073aabe14f3f174
temptado <jason1287712@gmail.com>
2021-09-03 20:20:24 -0400

    added comments to server.js file

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
+// Require express in order to run server
 const express = require('express');
+// Import routes
 const routes = require('./routes');
-const sequelize = require('./config/connection');
 // import sequelize connection
+const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
