2c985c7e538fdfa3985360a8ea24280a94c58c92
temptado <jason1287712@gmail.com>
2021-09-03 20:02:58 -0400

    added comments to the index of the api folder

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,13 @@
 const router = require('express').Router();
+// Import all the routes
 const categoryRoutes = require('./category-routes');
 const productRoutes = require('./product-routes');
 const tagRoutes = require('./tag-routes');
 
+// Define the path for all the routes that are being used
 router.use('/categories', categoryRoutes);
 router.use('/products', productRoutes);
 router.use('/tags', tagRoutes);
 
+// Export the router to be used in the index
 module.exports = router;
