c9c8ca91235d051c6e00826dc32f6b1335147dfc
temptado <jason1287712@gmail.com>
2021-09-03 20:04:26 -0400

    added comments to index of routes folder

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
 const router = require('express').Router();
+// Import all of the routes in the api folder
 const apiRoutes = require('./api');
 
+// Define the path for the api routes 
 router.use('/api', apiRoutes);
 
 router.use((req, res) => {
   res.send("<h1>Wrong Route!</h1>")
 });
 
+// Export the router in order ot be used in the server.js file
 module.exports = router;
